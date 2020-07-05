51e46211f42910b56763ea84fc9bcf95d2e46902
Terry Zhao <zhao.terryy@gmail.com>
2020-07-05 18:59:05 -0400

    no need for "selectedSeatsCount" - harder to read

--- script.js
+++ script.js
@@ -18,13 +18,12 @@ function setMovieData(movieIndex, moviePrice) {
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-  const selectedSeatsCount = selectedSeats.length;
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
   sessionStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
-  count.textContent = selectedSeatsCount;
-  total.textContent = selectedSeatsCount * ticketPrice;
+  count.textContent = selectedSeats.length;
+  total.textContent = selectedSeats.length * ticketPrice;
 }
 
 // Get data from sessionStorage and populate UI
